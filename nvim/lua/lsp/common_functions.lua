47462a9fe1c53db2c76098c39f73fe7a1f3638ea
Xiaoming <mail@zyming.me>
2026-04-04 00:03:20 +0200

    Disable lua_ls formatter

--- nvim/lua/lsp/common_functions.lua
+++ nvim/lua/lsp/common_functions.lua
@@ -2,7 +2,7 @@ local lsp = vim.lsp
 
 -- Code snippet stolen from neovim documentation
 
-vim.api.nvim_create_autocmd("LspAttach",{
+vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local bufnr = args.buf
         local client = lsp.get_client_by_id(args.data.client_id)
@@ -16,11 +16,11 @@ vim.api.nvim_create_autocmd("LspAttach",{
             vim.lsp.buf.hover()
         end)
         if client.supports_method('textDocument/formatting') then
-        -- Format the current buffer on save
+            -- Format the current buffer on save
             vim.api.nvim_create_autocmd('BufWritePre', {
                 buffer = args.buf,
                 callback = function()
-                    vim.lsp.buf.format({bufnr = args.buf, id = client.id})
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, filter = function(c) return c.name ~= "lua_ls" end })
                 end,
             })
         end
--- nvim/lua/lsp/formatter.lua
+++ nvim/lua/lsp/formatter.lua
@@ -2,5 +2,7 @@ require("conform").setup({
     formatter_by_ft = {
         lua = { "stylua" },
         rust = { "rustfmt", lsp_format = "fallback" },
-    }
+    },
+
+    format_on_save = {},
 })

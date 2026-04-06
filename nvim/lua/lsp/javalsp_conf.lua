c763f7678ebc36e6638b6ea9fe49cd56f12bd77b
Xiaoming <mail@zyming.me>
2026-04-06 12:44:48 +0200

    Change the java_lsp config to use nvim-jdtls

--- nvim/lua/lsp/javalsp_conf.lua
+++ nvim/lua/lsp/javalsp_conf.lua
@@ -1,14 +1,25 @@
-vim.lsp.config("jdtls", {
-	cmd = { "jdtls" },
-	root_markers = {
-		"gradlew",
-		"mvnw",
-		"pom.xml",
-		"build.gradle",
-		"build.gradle.kts",
-		".git",
-	},
-})
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function()
+		local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+		local workspace_dir = vim.fn.expand("~/.cache/jdtls/") .. project_name
+		local lombok_jar = vim.fn.expand("~/.local/lib/lombok.jar")
 
--- enable jdtls
-vim.lsp.enable("jdtls")
+		local cmd = { "jdtls", "-data", workspace_dir }
+		if vim.fn.filereadable(lombok_jar) == 1 then
+			vim.list_extend(cmd, { "--jvm-arg=-javaagent:" .. lombok_jar })
+		end
+
+		require("jdtls").start_or_attach({
+			cmd = cmd,
+			root_dir = require("jdtls.setup").find_root({
+				"gradlew",
+				"mvnw",
+				"pom.xml",
+				"build.gradle",
+				"build.gradle.kts",
+				".git",
+			}),
+		})
+	end,
+})
